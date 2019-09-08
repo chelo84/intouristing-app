53dc640cd73d12da6387aa506a252c7e1203e155
Chelo84 <marcelo_lacroix@hotmail.com>
2019-09-08 18:20:09 -0300

    initial commit

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,96 @@
+<template>
+    <q-layout view="hHh Lpr lff"  class="shadow-2 rounded-borders">
+      <q-header elevated class="dark-purple">
+        <q-toolbar>
+          <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
+          <!-- <q-toolbar-title>Header</q-toolbar-title> -->
+        </q-toolbar>
+      </q-header>
+
+      <q-drawer
+        v-model="drawer"
+        show-if-above
+
+        :mini="miniState"
+        @mouseover="miniState = false"
+        @mouseout="miniState = true"
+
+        :width="200"
+        :breakpoint="500"
+        bordered
+        content-class="bg-grey-3"
+      >
+        <q-scroll-area class="fit">
+          <q-list padding>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="img:https://cdn.quasar.dev/img/boy-avatar.png" />
+              </q-item-section>
+
+              <q-item-section>
+                User name
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="inbox" />
+              </q-item-section>
+
+              <q-item-section>
+                Inbox
+              </q-item-section>
+            </q-item>
+
+            <q-item active clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="star" />
+              </q-item-section>
+
+              <q-item-section>
+                Star
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="send" />
+              </q-item-section>
+
+              <q-item-section>
+                Send
+              </q-item-section>
+            </q-item>
+
+            <q-separator />
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="drafts" />
+              </q-item-section>
+
+              <q-item-section>
+                Drafts
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
+      </q-drawer>
+
+      <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    </q-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      miniState: true,
+    };
+  },
+};
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="shadow-13" style="padding: 2% 2% 2% 2%; width: 500px;">
+      <div class="row flex flex-center login-field">
+        <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" style="height: 200px;">
+      </div>
+      <div class="row flex flex-center login-field">
+        <q-input class="col-10" v-model="text" :label="$t('username')" :dense="dense"
+        color="dark-purple" />
+      </div>
+      <div class="row flex flex-center login-field">
+        <q-input v-model="password" type="password" class="col-10" :label="$t('password')"/>
+      </div>
+      <div class="row flex">
+        <a href="#" class="col-3 offset-4 self-center">Forgot password</a>
+        <q-btn color="dark-purple" class="col-2 offset-3" :label="$t('login')" />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+};
+</script>
+
+<style scoped>
+.login-field {
+  padding-bottom: 20px;
+  width: 100%;
+}
+</style>
