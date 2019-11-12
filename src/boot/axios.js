959fb4f10316fb374ae4192cc1b83863b7e8713e
Chelo84 <marcelo_lacroix@hotmail.com>
2019-11-11 22:17:20 -0300

    show notifications only if > 1

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -3,7 +3,7 @@ import { LocalStorage } from 'quasar';
 import { i18n } from './i18n';
 
 export default async ({ Vue }) => {
-  axios.defaults.headers.common.Authorization = LocalStorage.getItem('accessToken');
+  axios.defaults.headers.common.Authorization = String(LocalStorage.getItem('accessToken'));
   axios.defaults.baseURL = process.env.API_URL;
   axios.defaults.headers.common['Accept-Language'] = i18n.locale;
 
