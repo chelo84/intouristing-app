b570cea6523b47089006bb50cd67cf2d4f1ce376
Chelo84 <marcelo_lacroix@hotmail.com>
2019-11-24 22:47:09 -0300

    heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const
 const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
